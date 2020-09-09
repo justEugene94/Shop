4f9c31bd90b2bbc3bb6b30eeefaae290848249be
Eugene <drydude77@gmail.com>
2020-09-09 21:42:01 +0300

    Added Goods model

---
+++ app/Models/Goods.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Goods extends Model
+{
+    protected $table = 'goods';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'quantity',
+    ];
+}
