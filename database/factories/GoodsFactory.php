7accba316c676614a7af8ec90e495be545c17d04
Eugene <drydude77@gmail.com>
2020-09-09 22:00:37 +0300

    Fixed title in GoodsFactory

--- database/factories/GoodsFactory.php
+++ database/factories/GoodsFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Goods::class, function (Faker $faker) {
     return [
-        'title' => $faker->title,
+        'title' => $faker->text(20),
         'description' => $faker->text(200),
         'price' => $faker->numberBetween(100, 400),
         'quantity' => $faker->numberBetween(0, 900)
