635dca1042f47cf6d8699770a6aec574be51f4e6
Eugene <drydude77@gmail.com>
2020-09-09 21:39:03 +0300

    Added goods table

---
+++ database/migrations/2020_09_09_182810_create_goods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('quantity');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
