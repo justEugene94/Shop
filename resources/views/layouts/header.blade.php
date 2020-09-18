74295c77095c8d75ad6d3d7052bf7bacc0ecf017
Eugene <drydude77@gmail.com>
2020-09-19 00:22:23 +0300

    Added styles for shopping cart in layouts/header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,8 +12,86 @@
                 <span class="cart-total">({{ session()->get('qty', 0) }})</span>
             </button>
             <div class="dropdown-content">
-                <h5 class="modal-title">Your cart is empty</h5>
-                <p>Time to start shopping</p>
+                @if(session()->get('qty', 0) == 0)
+                    <h5 class="modal-title">Your shopping cart is empty</h5>
+                    <p>Time to start shopping</p>
+                @else
+                    <h1>Shopping Cart</h1>
+
+                    <div class="shopping-cart">
+
+                        <div class="column-labels">
+                            <label class="product-image">Image</label>
+                            <label class="product-details">Product</label>
+                            <label class="product-price">Price</label>
+                            <label class="product-quantity">Quantity</label>
+                            <label class="product-removal">Remove</label>
+                            <label class="product-line-price">Total</label>
+                        </div>
+
+                        <div class="product">
+                            <div class="product-image">
+                                <img class="card-img-top img-fluid" src="http://placehold.it/200x200" alt="">
+                            </div>
+                            <div class="product-details">
+                                <div class="product-title">Nike Flex Form TR Women's Sneaker</div>
+                            </div>
+                            <div class="product-price">12.99</div>
+                            <div class="product-quantity">
+                                <input type="number" value="2" min="1">
+                            </div>
+                            <div class="product-removal">
+                                <button class="remove-product">
+                                    Remove
+                                </button>
+                            </div>
+                            <div class="product-line-price">25.98</div>
+                        </div>
+
+                        <div class="product">
+                            <div class="product-image">
+                                <img src="images/adidas.jpg">
+                            </div>
+                            <div class="product-details">
+                                <div class="product-title">ULTRABOOST UNCAGED SHOES</div>
+                                <p class="product-description">Born from running culture, these men's shoes deliver the
+                                    freedom of a cage-free design</p>
+                            </div>
+                            <div class="product-price">45.99</div>
+                            <div class="product-quantity">
+                                <input type="number" value="1" min="1">
+                            </div>
+                            <div class="product-removal">
+                                <button class="remove-product">
+                                    Remove
+                                </button>
+                            </div>
+                            <div class="product-line-price">45.99</div>
+                        </div>
+
+                        <div class="totals">
+                            <div class="totals-item">
+                                <label>Subtotal</label>
+                                <div class="totals-value" id="cart-subtotal">71.97</div>
+                            </div>
+                            <div class="totals-item">
+                                <label>Tax (5%)</label>
+                                <div class="totals-value" id="cart-tax">3.60</div>
+                            </div>
+                            <div class="totals-item">
+                                <label>Shipping</label>
+                                <div class="totals-value" id="cart-shipping">15.00</div>
+                            </div>
+                            <div class="totals-item totals-item-total">
+                                <label>Grand Total</label>
+                                <div class="totals-value" id="cart-total">90.57</div>
+                            </div>
+                        </div>
+
+                        <button class="checkout">Checkout</button>
+
+                    </div>
+                @endif
             </div>
 
         </div>
