a9fd8d8401406bda0b91be12202bce9f2f2a3b7d
Eugene <drydude77@gmail.com>
2020-09-25 19:32:01 +0300

    Changed inputs for checkout page

--- resources/views/checkout/content.blade.php
+++ resources/views/checkout/content.blade.php
@@ -55,26 +55,26 @@
                     </div>
                 </div>
 
-                <div class="mb-3">
-                    <label for="username">Username</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">@</span>
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="mobile_phone">Mobile Phone</label>
+                        <input type="text" class="form-control" id="mobile_phone" placeholder="" value="" required>
+                        <div class="invalid-feedback">
+                            Valid mobile phone is required.
                         </div>
-                        <input type="text" class="form-control" id="username" placeholder="Username" required>
-                        <div class="invalid-feedback" style="width: 100%;">
-                            Your username is required.
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="" value="" required>
+                        <div class="invalid-feedback">
+                            Please enter a valid email address for shipping updates.
                         </div>
                     </div>
                 </div>
 
-                <div class="mb-3">
-                    <label for="email">Email <span class="text-muted">(Optional)</span></label>
-                    <input type="email" class="form-control" id="email" placeholder="you@example.com">
-                    <div class="invalid-feedback">
-                        Please enter a valid email address for shipping updates.
-                    </div>
-                </div>
+                <hr class="mb-4">
+
+                <h4 class="mb-3">Pickup from Nova Poshta</h4>
 
                 <div class="mb-3">
                     <label for="address">Address</label>
@@ -91,8 +91,8 @@
 
                 <div class="row">
                     <div class="col-md-5 mb-3">
-                        <label for="country">Country</label>
-                        <select class="custom-select d-block w-100" id="country" required>
+                        <label for="city">City</label>
+                        <select class="custom-select d-block w-100" id="city" required>
                             <option value="">Choose...</option>
                             <option>United States</option>
                         </select>
