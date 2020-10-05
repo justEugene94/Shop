3733f7267e22c6da949377b60e262906a361e463
Eugene <drydude77@gmail.com>
2020-10-05 18:39:13 +0300

    Added method for route checkout.store

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,10 +4,21 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\Checkout\StoreFormRequest;
+use Illuminate\Contracts\View\View;
+
 class CheckoutController extends Controller
 {
-    public function index()
+    /**
+     * @return View
+     */
+    public function index(): View
     {
         return view('checkout');
+    }
+
+    public function store(StoreFormRequest $request)
+    {
+        return '';
     }
 }
