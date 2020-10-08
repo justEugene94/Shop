8ef8a223425926721073961fc22784cbafaa68e7
Eugene <drydude77@gmail.com>
2020-10-08 16:43:08 +0300

    Added Service for CartController

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Goods;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Ramsey\Collection\Exception\NoSuchElementException;
@@ -18,12 +19,12 @@ class CartController extends Controller
      *
      * @return Response
      */
-    public function add(Request $request, int $goods_id)
+    public function add(Request $request, CartService $service, int $goods_id)
     {
         $product = Goods::query()->findOrFail($goods_id);
 
         if (!$request->session()->has('cart')) {
-            $cart = $this->addProductInCartArray($product);
+            $cart = $service->addProductInCartArray($product);
 
             $request->session()->put('cart', $cart);
         } elseif ($request->session()->has("cart.{$goods_id}")) {
@@ -33,7 +34,7 @@ class CartController extends Controller
             session()->put('cart', $cart);
         } else {
             $cart = $request->session()->get('cart');
-            $cart = $this->addProductInCartArray($product, $cart);
+            $cart = $service->addProductInCartArray($product, $cart);
             $request->session()->put('cart', $cart);
         }
 
@@ -46,7 +47,7 @@ class CartController extends Controller
         $request->session()->put('price', $price);
 
         return response([
-            'content' => $this->htmlContent($request->session()->get('cart'), $price),
+            'content' => $service->getHtmlContent($request->session()->get('cart'), $price),
             'qty' => $qty,
             'price' => $price,
         ],200);
@@ -84,119 +85,5 @@ class CartController extends Controller
             'price' => $price,
             'qty' => $qty,
         ], 200);
-    }
-
-    /**
-     * @param Goods $product
-     * @param array $cart
-     *
-     * @return array
-     */
-    private function addProductInCartArray(Goods $product, Array $cart = []) :array
-    {
-        $cart[$product->id] = [
-            'name' => $product->title,
-            'price' => $product->price,
-            'quantity' => 1,
-            'total' => $product->price,
-        ];
-
-        return $cart;
-    }
-
-    /**
-     * @param array $cart
-     * @param int $price
-     *
-     * @return string
-     */
-    private function htmlContent(array $cart, int $price) :string
-    {
-        $result = "";
-
-        $result = $this->addCartHeader($result);
-
-        $result = $this->addCartProducts($result);
-
-        $result = $this->addCartTotal($result, $price);
-
-       return $result;
-    }
-
-    /**
-     * @param string $result
-     * @return string
-     */
-    private function addCartHeader(string $result): string
-    {
-        $result .= "<h2>Shopping Cart</h2>
-
-                    <div class='shopping-cart'>
-
-                        <div class='column-labels'>
-                            <label class='product-image'>Image</label>
-                            <label class='product-details'>Product</label>
-                            <label class='product-price'>Price</label>
-                            <label class='product-quantity'>Quantity</label>
-                            <label class='product-removal'>Remove</label>
-                            <label class='product-line-price'>Total</label>
-                        </div>";
-
-        return $result
-    }
-
-    /**
-     * @param string $result
-     * @return string
-     */
-    private function addCartProducts(string $result): string
-    {
-        foreach ($cart as $id => $product) {
-            $result .= "<div class='product'>
-                    <div class='product-image'>
-                        <img class='card-img-top img-fluid' src='http://placehold.it/200x200' alt=''>
-                    </div>
-                    <div class='product-details'>
-                        <div class='product-title'>{$product['name']}</div>
-                    </div>
-                    <div class='product-price'>{$product['price']}</div>
-                    <div class='product-quantity'>
-                        <input type='number' value='{$product['quantity']}' min='1'>
-                    </div>
-                    <div class='product-removal'>
-                        <form class='delete' action='' method='DELETE'>
-                                    <input type='hidden' name='id' value='{{ $id }}'>
-                                    <button type='submit' class='remove-product'>
-                                        Remove
-                                    </button>
-                                </form>
-                    </div>
-                    <div class='product-line-price'>{$product['total']}</div>
-               </div>";
-        }
-
-        return $result
-    }
-
-    /**
-     * @param string $result
-     * @param int $price
-     *
-     * @return string
-     */
-    private function addCartTotal(string $result, int $price): string
-    {
-        $result .= "<div class='totals'>
-                            <div class='totals-item totals-item-total'>
-                                <label>Total</label>
-                                <div class='totals-value' id='cart-total'>{$price}</div>
-                            </div>
-                        </div>
-
-                        <a class='checkout' href='" . route('checkout')  ."'>Checkout</a>
-
-                    </div>";
-
-        return $result;
     }
 }
---
+++ app/Services/CartService.php
@@ -0,0 +1,126 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Models\Goods;
+
+class CartService
+{
+    /**
+     * @param Goods $product
+     * @param array $cart
+     *
+     * @return array
+     */
+    public function addProductInCartArray(Goods $product, Array $cart = []) :array
+    {
+        $cart[$product->id] = [
+            'name' => $product->title,
+            'price' => $product->price,
+            'quantity' => 1,
+            'total' => $product->price,
+        ];
+
+        return $cart;
+    }
+
+    /**
+     * @param array $cart
+     * @param int $price
+     *
+     * @return string
+     */
+    public function getHtmlContent(array $cart, int $price) :string
+    {
+        $result = "";
+
+        $result = $this->addCartHeader($result);
+
+        $result = $this->addCartProducts($result, $cart);
+
+        $result = $this->addCartTotal($result, $price);
+
+        return $result;
+    }
+
+    /**
+     * @param string $result
+     * @return string
+     */
+    private function addCartHeader(string $result): string
+    {
+        $result .= "<h2>Shopping Cart</h2>
+
+                    <div class='shopping-cart'>
+
+                        <div class='column-labels'>
+                            <label class='product-image'>Image</label>
+                            <label class='product-details'>Product</label>
+                            <label class='product-price'>Price</label>
+                            <label class='product-quantity'>Quantity</label>
+                            <label class='product-removal'>Remove</label>
+                            <label class='product-line-price'>Total</label>
+                        </div>";
+
+        return $result;
+    }
+
+    /**
+     * @param string $result
+     * @param array $cart
+     *
+     * @return string
+     */
+    private function addCartProducts(string $result, array $cart): string
+    {
+        foreach ($cart as $id => $product) {
+            $result .= "<div class='product'>
+                    <div class='product-image'>
+                        <img class='card-img-top img-fluid' src='http://placehold.it/200x200' alt=''>
+                    </div>
+                    <div class='product-details'>
+                        <div class='product-title'>{$product['name']}</div>
+                    </div>
+                    <div class='product-price'>{$product['price']}</div>
+                    <div class='product-quantity'>
+                        <input type='number' value='{$product['quantity']}' min='1'>
+                    </div>
+                    <div class='product-removal'>
+                        <form class='delete' action='' method='DELETE'>
+                                    <input type='hidden' name='id' value='{{ $id }}'>
+                                    <button type='submit' class='remove-product'>
+                                        Remove
+                                    </button>
+                                </form>
+                    </div>
+                    <div class='product-line-price'>{$product['total']}</div>
+               </div>";
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $result
+     * @param int $price
+     *
+     * @return string
+     */
+    private function addCartTotal(string $result, int $price): string
+    {
+        $result .= "<div class='totals'>
+                            <div class='totals-item totals-item-total'>
+                                <label>Total</label>
+                                <div class='totals-value' id='cart-total'>{$price}</div>
+                            </div>
+                        </div>
+
+                        <a class='checkout' href='" . route('checkout')  ."'>Checkout</a>
+
+                    </div>";
+
+        return $result;
+    }
+}
