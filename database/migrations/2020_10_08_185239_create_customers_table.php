412758460415c2a4d46f589339725c251627e9d1
Eugene <drydude77@gmail.com>
2020-10-08 21:58:21 +0300

    Added migration for customers table

---
+++ database/migrations/2020_10_08_185239_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number')->unique();
+            $table->string('email');
+            $table->string('stripe_customer_id', 255)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
