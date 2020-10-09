0fe63f25acab9983c0c0c14ea374b73082f76ee1
Eugene <drydude77@gmail.com>
2020-10-09 21:38:36 +0300

    Added PHPDOC for Models Customer and City

--- app/Models/City.php
+++ app/Models/City.php
@@ -6,10 +6,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $np_id
+ * @property string $name
+ */
 class City extends Model
 {
+    /** @var string  */
     protected $table = 'cities';
 
+    /** @var array */
     protected $fillable = [
         'np_id',
         'name',
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,11 +6,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $phone_number
+ * @property string $email
+ * @property int $stripe_customer_id
+ */
 class Customer extends Model
 {
     /** @var string */
     protected $table = 'customers';
 
+    /** @var array  */
     protected $fillable = [
         'first_name',
         'last_name',
