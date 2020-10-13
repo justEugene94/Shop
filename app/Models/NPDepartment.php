9fa260aebf31268cf608f63d8a9a094f9268e3af
Eugene <drydude77@gmail.com>
2020-10-13 23:35:05 +0300

    Added relation in NPDepartment for Order

--- app/Models/NPDepartment.php
+++ app/Models/NPDepartment.php
@@ -7,7 +7,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property string np_id
+ * @property string department
+ *
+ * Relations:
+ * @property City $city
+ * @property Customer[] $customers
+ * @property Order[] $orders
+ */
 class NPDepartment extends Model
 {
     /** @var string  */
@@ -33,5 +43,13 @@ class NPDepartment extends Model
     public function customers()
     {
         return $this->belongsToMany(Customer::class, 'customer_np_department', 'np_department_id', 'customer_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'np_department_id', 'id');
     }
 }
