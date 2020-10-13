1b2bb82c3e5898e500b219e4a75c1e5f9bc73278
Eugene <drydude77@gmail.com>
2020-10-13 20:50:32 +0300

    Added table customer_np_department

---
+++ database/migrations/2020_10_13_171241_create_user_np_department_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerNpDepartmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_np_department', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('np_department_id');
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('np_department_id')->references('id')->on('np_departments');
+
+            $table->unique(['customer_id', 'np_department_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_np_department');
+    }
+}
