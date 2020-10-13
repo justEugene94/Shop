76de7b4491179f640fc28b608e0389d315863de4
Eugene <drydude77@gmail.com>
2020-10-13 23:29:42 +0300

    Added model Order

---
+++ app/Models/Order.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property string $stripe_order_id
+ * @property float $amount
+ * @property int $status_id
+ *
+ * Relationships:
+ * @property Customer $customer
+ * @property NPDepartment $npDepartment
+ * @property Status $status
+ */
+class Order extends Model
+{
+    /** @var string */
+    protected $table = 'orders';
+
+    /** @var array  */
+    protected $fillable = [
+        'stripe_order_id',
+        'amount',
+        'status_id'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function npDepartment()
+    {
+        return $this->belongsTo(NPDepartment::class, 'np_department_id', 'id');
+    }
+
+    /**
+     * @return Status
+     */
+    public function getStatusAttribute()
+    {
+        return (new Status)->find($this->status_id);
+    }
+}
