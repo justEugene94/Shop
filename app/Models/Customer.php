473fe009a7005f0205d4d3fe6ae683154acf85e8
Eugene <drydude77@gmail.com>
2020-10-13 23:32:03 +0300

    Added relation in Customer for Order

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property int $id
@@ -14,6 +15,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  * @property string $phone_number
  * @property string $email
  * @property int $stripe_customer_id
+ *
+ * Relationship:
+ * @property NPDepartment[] $npDepartments
+ * @property Order[] $orders
  */
 class Customer extends Model
 {
@@ -35,5 +40,13 @@ class Customer extends Model
     public function npDepartments()
     {
         return $this->belongsToMany(NPDepartment::class, 'customer_np_department', 'customer_id', 'np_department_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'customer_id', 'id');
     }
 }
