ea291c09d550352eb431d4dbeb5f13e8f2ddbafc
Eugene <drydude77@gmail.com>
2020-10-13 23:10:20 +0300

    Added table orders

---
+++ database/migrations/2020_10_13_183509_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('np_department_id');
+            $table->string('stripe_order_id');
+            $table->unsignedDecimal('amount');
+
+            $table->unsignedInteger('status_id')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
