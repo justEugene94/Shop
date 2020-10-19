d350f125e13445e3c58d050357ef2fe0310b2589
Eugene <drydude77@gmail.com>
2020-10-19 23:06:53 +0300

    Added method create for CustomerService

---
+++ app/Services/CustomerService.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Models\Customer;
+
+class CustomerService
+{
+    /**
+     * @param string $first_name
+     * @param string $last_name
+     * @param string $email
+     * @param string $phoneNumber
+     * 
+     * @return Customer
+     */
+    public function create(string $first_name, string $last_name, string $email, string $phoneNumber): Customer
+    {
+        /** @var Customer $customer */
+        $customer = Customer::query()->firstOrCreate(['phone_number' => $phoneNumber], [
+            'first_name'  => $first_name,
+            'last_name'   => $last_name,
+            'email'       => $email,
+            'phoneNumber' => $phoneNumber,
+        ]);
+
+        return $customer;
+    }
+}
