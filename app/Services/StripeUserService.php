7a7704d22fc800bed6bc0f1c7319d7d0d4862e1d
Eugene <drydude77@gmail.com>
2020-10-19 19:23:42 +0300

    Added method checkDetails

--- app/Services/StripeUserService.php
+++ app/Services/StripeUserService.php
@@ -10,19 +10,21 @@ class StripeUserService extends StripeService
 {
     /**
      * @param Customer $customer
-     * 
+     *
      * @return Stripe\Customer
      * @throws Stripe\Exception\ApiErrorException
      */
     public function get(Customer $customer): Stripe\Customer
     {
-        $customerId = $customer->stripe_customer_id;
+        $stripeCustomerId = $customer->stripe_customer_id;
 
-        if ($customerId)
-        {
+        if ($stripeCustomerId) {
+            $stripeCustomer = Stripe\Customer::retrieve($stripeCustomerId);
 
-        }
-        else
+            if (!$this->checkDetails($customer, $stripeCustomer)) {
+
+            }
+        } else
             return $this->create($customer);
     }
 
@@ -45,5 +47,26 @@ class StripeUserService extends StripeService
         $customer->save();
 
         return $stripeCustomer;
+    }
+
+    /**
+     * @param Customer $customer
+     * @param Stripe\Customer $stripeCustomer
+     * 
+     * @return bool
+     */
+    public function checkDetails(Customer $customer, Stripe\Customer $stripeCustomer): bool
+    {
+        if (
+            $customer->email == $stripeCustomer->email
+            ||
+            $customer->phone_number == $stripeCustomer->phone
+            ||
+            "{$customer->first_name} {$customer->last_name}" == $stripeCustomer->name
+        ) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
