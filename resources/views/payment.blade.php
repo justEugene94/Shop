60e43de10236949ae3ef391958ad5fb65627c512
Eugene <drydude77@gmail.com>
2020-10-30 17:21:25 +0200

    Added view for payment page

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -8,7 +8,6 @@
     @include('layouts.header')
 @endsection
 
-
 @section('content')
     @include('payment.content')
 @endsection
@@ -19,4 +18,53 @@
 
 @section('scripts')
     <script src="{{ url('/js/logic-for-cart.js') }}"></script>
+    <script>
+        let stripe = Stripe('{{ env('STRIPE_KEY') }}');
+        let elements = stripe.elements();
+        let style = {
+            base: {
+                color: "#32325d",
+            }
+        };
+
+        let card = elements.create("card", { style: style });
+        card.mount("#card-element");
+
+        card.on('change', function(event) {
+            let displayError = document.getElementById('card-errors');
+            if (event.error) {
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+
+        let form = document.getElementById('payment-form');
+
+        form.addEventListener('submit', function(ev) {
+            ev.preventDefault();
+            stripe.confirmCardPayment('{{ $client_secret }}', {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: '{{ $first_name }} {{ $last_name }}'
+                    }
+                }
+            }).then(function(result) {
+                if (result.error) {
+                    // Show error to your customer (e.g., insufficient funds)
+                    $('#container').prepend('<div class="alert alert-danger" role="alert">' +
+                                                        result.error.message
+                                                    +'</div>')
+                    console.log(result.error.message);
+                } else {
+                    // The payment has been processed!
+                    if (result.paymentIntent.status === 'succeeded') {
+                        console.log(result.paymentIntent)
+                        {{--window.location.href = "{{URL::to('/thankyou')}}"--}}
+                    }
+                }
+            });
+        });
+    </script>
 @endsection
