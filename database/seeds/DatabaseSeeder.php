e1ad6380b3487bc12fe66f8161ec9af4baa192a8
Eugene <drydude77@gmail.com>
2020-11-02 19:11:27 +0200

    Added AdminTablesSeeder and fixed login for admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+         $this->call(AdminTablesSeeder::class);
         $this->call(ProductsSeeder::class);
     }
 }
