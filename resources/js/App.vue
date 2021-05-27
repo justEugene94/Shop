27ec6d76869a9fbcf9c005efb0cd87d62ac84a7a
Eugene <drydude77@gmail.com>
2021-05-27 21:54:52 +0300

    Added layout in App.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,13 +1,75 @@
 <template>
-    <v-app>{{ text }}</v-app>
+    <v-app>
+        <v-navigation-drawer
+            permanent
+            expand-on-hover
+            clipped
+            app
+            dark
+            color="deep-purple"
+        >
+            <v-list>
+                <v-list-item
+                    v-for="link in links"
+                    :key="link.title"
+                    link
+                >
+                    <v-list-item-icon>
+                        <v-icon>
+                            {{ link.icon }}
+                        </v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title v-text="link.title"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+            app
+            clipped-left
+            dark
+            color="deep-purple"
+        >
+            <v-toolbar-title>
+                  <span
+                      role="link"
+                  >
+                    Here can be the name of your store
+                  </span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-cart-outline</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-main>
+            <v-container fluid>
+
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 export default {
     name: 'App',
     data: () => {
-        return {
-            text: 'Hello World!'
+        return {}
+    },
+    computed: {
+        links () {
+            return [
+                {title: 'Home', icon: 'mdi-home-outline', url: '/'},
+                {title: 'Products', icon: 'mdi-shopping-outline', url: '/products'},
+                {title: 'About us', icon: 'mdi-information-outline', url: '/about'},
+                {title: 'Contacts', icon: 'mdi-phone-outline', url: '/contacts'},
+            ]
         }
     }
 }
