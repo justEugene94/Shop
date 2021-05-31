e9f5ce237aadd86fc508253e41d6a82a5040e97c
Eugene <drydude77@gmail.com>
2021-05-31 21:25:20 +0300

    Added layout for Cart.vue component

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -1,10 +1,67 @@
 <template>
-    <div>Cart page</div>
+    <v-list three-line>
+        <template v-for="(product, index) in products">
+
+            <v-list-item
+                :key="product.title"
+            >
+                <v-list-item-avatar
+                    height="120"
+                    width="140"
+                    rounded
+                >
+                    <v-img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                    ></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                    <v-list-item-title v-html="product.title"></v-list-item-title>
+                    <v-list-item-subtitle v-html="product.subtitle"></v-list-item-subtitle>
+                </v-list-item-content>
+
+            </v-list-item>
+
+            <v-divider
+                v-if="index + 1 < products.length"
+                :key="index"
+            ></v-divider>
+
+        </template>
+    </v-list>
 </template>
 
 <script>
 export default {
-    name: 'Cart'
+    name: 'Cart',
+    data: () => ({
+        products: [
+            {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                title: 'Brunch this weekend?',
+                subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+            },
+            {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+                subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+            },
+            {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                title: 'Oui oui',
+                subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+            },
+            {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                title: 'Birthday gift',
+                subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+            },
+            {
+                avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                title: 'Recipe to try',
+                subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+            },
+        ],
+    }),
 }
 </script>
 
