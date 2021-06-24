db4ead59aa8fb751451c5dd7173bfae9cd35949a
Eugene <drydude77@gmail.com>
2021-06-24 20:11:41 +0300

    Fixed default param for column status_id in orders table

--- database/migrations/2020_10_13_183509_create_orders_table.php
+++ database/migrations/2020_10_13_183509_create_orders_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Status;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,7 +22,7 @@ class CreateOrdersTable extends Migration
             $table->string('stripe_order_id');
             $table->unsignedDecimal('amount');
 
-            $table->unsignedInteger('status_id')->default(1);
+            $table->unsignedInteger('status_id')->default(Status::CREATED);
             $table->text('info');
 
             $table->foreign('customer_id')->references('id')->on('customers');
