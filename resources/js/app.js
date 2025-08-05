b344b844e2f190f26f161703e779ad1dc8de4337
justEugene94 <drydude77@gmail.com>
2025-08-05 18:31:11 +0300

    Fixed App import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import Vue        from 'vue'
 import VueCookies from 'vue-cookies'
 
-import App        from './App'
+import App        from './App.vue'
 import vuetify    from './plugins/vuetify'
 import router     from './router'
 import store      from './store'
