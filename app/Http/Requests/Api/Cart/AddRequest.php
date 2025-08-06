032806d5102e1f68cd5949aa4b30aa8c129a6779
justEugene94 <drydude77@gmail.com>
2025-08-06 19:47:49 +0300

    Added feature tests for api.cart

--- app/Http/Requests/Api/Cart/AddRequest.php
+++ app/Http/Requests/Api/Cart/AddRequest.php
@@ -55,6 +55,6 @@ class AddRequest extends CartRequest
      */
     public function getRewrite(): bool
     {
-        return $this->input('rewrite');
+        return $this->input('rewrite', false);
     }
 }
